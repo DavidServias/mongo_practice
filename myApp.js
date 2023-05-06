2e5601a7305d612923a027eabc5aad2127561c7d
David Servias <davidservias@gmail.com>
2023-05-05 21:59:18 -0700

    connect to db

--- myApp.js
+++ myApp.js
@@ -1,5 +1,15 @@
 require('dotenv').config();
 
+/** 1) Install & Set up mongoose */
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(function(result) {
+    console.log('Database is connected');
+  })
+  .catch((err) => console.log(err));
+;
+
+
 
 let Person;
 
