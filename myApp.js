7926464cdf576d6eb121944063ebd043f88ff941
David Servias <davidservias@gmail.com>
2023-05-06 14:26:42 -0700

    added error message for db connection fail

--- myApp.js
+++ myApp.js
@@ -6,7 +6,10 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
   .then(function(result) {
     console.log('Database is connected');
   })
-  .catch((err) => console.log(err));
+  .catch((err) => {
+    console.log('Database is not connected');
+    console.log(err);
+  });
 ;
 
 
