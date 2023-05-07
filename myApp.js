b205b61f4302549125decb3da2a6db931a5cac77
David Servias <davidservias@gmail.com>
2023-05-06 21:36:15 -0700

    define findEditThenSave()

--- myApp.js
+++ myApp.js
@@ -67,8 +67,15 @@ const findPersonById = (personId, done) => {
 
 const findEditThenSave = (personId, done) => {
   const foodToAdd = "hamburger";
-
-  done(null /*, data*/);
+  Person.findById(personId, function(err, data) { 
+    if (err) return console.error(err);
+    data.favoriteFoods.push(foodToAdd);
+    data.save(function(err, data) {
+      if (err) return console.error(err);
+      done(null, data);
+    });
+  }
+  
 };
 
 const findAndUpdate = (personName, done) => {
